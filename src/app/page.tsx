83ea946f2d60ea1d48d7c491ac08b1ca1ac864c8
Horquidia <horquidia.a.lima@gmail.com>
2025-07-29 16:06:16 -0300

    Criando Abas com Radix Tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { SettingsTabs } from "@/components/SettingsTabs";
+
 export default function Home() {
   return (
     <>
-      <h1>hello word</h1>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <SettingsTabs/>
     </>
   );
 }
