9b73e2ada66c0acd12a45aea3c1460965aa8e666
Horquidia <horquidia.a.lima@gmail.com>
2025-07-30 16:14:18 -0300

    Componentizando File Input

---
+++ src/components/Form/FileInput/index.tsx
@@ -0,0 +1,11 @@
+import { Control } from "./Control";
+import { ImagePreview } from "./ImagePreview";
+import { Trigger } from "./Trigger";
+import { Root } from "./Root";
+
+export {
+    Root,
+    Control,
+    ImagePreview,
+    Trigger,
+}
