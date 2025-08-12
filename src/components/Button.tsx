074c320ac6393ec60d1ef9ebc44e2a7265d2b07b
Horquidia <horquidia.a.lima@gmail.com>
2025-08-12 19:36:18 -0300

    Variantes com slots

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -13,7 +13,7 @@ const button = tv ({
     variants: {
         variant: {
             primary: 'bg-violet-600 text-white hover:bg-violet-700',
-            ghost: 'rounded-md px-2 hover:bg-zinc-50 shadow-none',
+            ghost: 'rounded-md px-2 hover:bg-zinc-50 shadow-none text-zinc-500',
             outline: 'border border-zinc-300 text-zinc-700 hover:bg-zinc-50',
         },
     },
@@ -23,11 +23,11 @@ const button = tv ({
     },
 })
 
-export function Button({variant, ...props}: ButtonProps) {
+export function Button({variant,className, ...props}: ButtonProps) {
     return(
         <button 
             {...props}
-            className={button({variant})}
+            className={button({variant, className})}
         />
     )
 }
